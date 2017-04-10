9c8665d4b8dd875174728e7c3448ebb4f64e905e
Sylvain CAU <ashdevfr@gmail.com>
2017-04-09 23:38:55 -0700

    :wrench: Default config Transmission / Muximux

---
+++ images/muximux/settings.ini.php
@@ -0,0 +1,93 @@
+; <?php die('Access denied'); ?>
+[general]
+title = "Muximux - Application Management Console"
+branch = "master"
+theme = "classic"
+color = "#31ac63"
+splashscreen = "true"
+tabcolor = "true"
+rss = "true"
+rssUrl = "https://www.wired.com/feed/"
+userNameInput = "admin"
+password = "muximux"
+
+[settings]
+branch_changed = ""
+sha = "18ecfb7a9205e719a93aa8cfb00530b449021239"
+enabled = "true"
+default = "false"
+name = "Settings"
+url = "muximux.php"
+landingpage = "false"
+icon = "fa-cog"
+dd = "true"
+branches[develop] = "988867561d1163d69ccf4a615d8129247faeca2c"
+branches[master] = "18ecfb7a9205e719a93aa8cfb00530b449021239"
+branches[parseError] = "14ea3581389e329e27c496367b2b85b15fc106f9"
+branches[saveProblem] = "4d16f3f4b1d7fb18bac5d8ed09b9a45dbe6711ff"
+last_check = 1491764703
+
+[Plex]
+name = "Plex"
+url = "http://plex.__SERVERNAME__/web/index.html"
+scale = 1
+icon = "muximux-plex"
+color = "#f9be03"
+enabled = "true"
+default = "true"
+
+[Transmission]
+name = "Transmission"
+url = "http://transmission.__SERVERNAME__"
+scale = 1
+icon = "muximux-utorrent"
+color = "#00ff00"
+enabled = "true"
+
+[RuTorrent]
+name = "Deluge"
+url = "http://deluge.__SERVERNAME__"
+scale = 1
+icon = "muximux-deluge"
+color = "#1a1bfe"
+enabled = "true"
+
+[Couchpotato]
+name = "CouchPotato"
+url = "http://couchpotato.__SERVERNAME__"
+scale = 1
+icon = "muximux-couchpotato"
+color = "#f85c22"
+enabled = "true"
+
+[Sickbeard]
+name = "Sickrage"
+url = "http://sickrage.__SERVERNAME__"
+scale = 1
+icon = "muximux-sickbeard"
+color = "#b6f000"
+enabled = "true"
+
+[Headphones]
+name = "Headphones"
+url = "http://headphones.__SERVERNAME__"
+scale = 1
+icon = "muximux-headphones"
+color = "#000000"
+enabled = "true"
+
+[Sonarr]
+name = "Sonarr"
+url = "http://sonarr.__SERVERNAME__"
+scale = 1
+icon = "muximux-sonarr"
+color = "#35c5f4"
+enabled = "true"
+
+[Explorer]
+name = "Explorer"
+url = "http://explorer.__SERVERNAME__"
+scale = 1
+icon = "muximux-download"
+color = "#3f8927"
+enabled = "true"
